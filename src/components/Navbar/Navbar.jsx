38e745c5116f6bf864fec0aa20f3a2a249fe00bd
BlackEmpir7199 <blackempire9177@gmail.com>
2024-07-16 15:00:09 +0530

    Added Responsive nav bar for all devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss"
 const Navbar = ()=> {
+  const[open,setOpen] = useState(false)
   return (
     <nav className="Navbar">
         <div className='left'>
@@ -16,6 +17,16 @@ const Navbar = ()=> {
         <div className='right'>
           <a href="/" className="SignIn">Sign In</a>
           <a href="/" className="SignUp">Sign Up</a>
+          <div className="menuIcon">
+            <img src="/menu.png" alt="logo" onClick={()=>setOpen((prev)=>!prev)}/></div>
+            <div className= {open ? "menu active":"menu"}>
+            <a href='/'>Home</a>
+            <a href='/'>About</a>
+            <a href='/'>Contact</a>
+            <a href='/'>Agents</a>
+            <a href="/">Sign In</a>
+            <a href="/">Sign Up</a>
+          </div>
         </div>
     </nav>
   )
