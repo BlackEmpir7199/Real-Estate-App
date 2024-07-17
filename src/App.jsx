6e7a798638eb12e3053cf917a3ecdaf5a67e95b7
BlackEmpir7199 <blackempire9177@gmail.com>
2024-07-18 02:11:34 +0530

    herosection implmentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar/Navbar"
 import "./layout.scss"
+import HomePage from "./routes/HomePage"
 
 function App() {
   return (
     <div className="layout">
-      <Navbar></Navbar>
+      <Navbar/>
+      <div className="homesection">
+        <HomePage/>
+      </div>
     </div>
 
   )
---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./HomePage.scss"
+const HomePage = () => {
+  return (
+    <div className='herosection'>
+        <div className='textcontainer'>
+            RealEstate React 
+        </div>
+        <div className='imgcontainer'>
+           <img src='/bg.png' alt='house'/>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
